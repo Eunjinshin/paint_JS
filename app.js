8b55d2ad54b8855fcb5316e1e42c5f08d9c14f8b
eunjin <eunjinshin97@gmail.com>
2021-07-08 15:28:03 +0900

    2D Context

--- app.js
+++ app.js
@@ -1,1 +1,47 @@
-const canvas =
+const canvas = document.getElementById("jsCanvas");
+const ctx = canvas.getContext("2d");
+
+canvas.width = 700;//canvas크기 설정
+canvas.height = 700;
+
+ctx.strokeStyle = "#2c2c2c";//첫번째 색 
+ctx.lineWidth = 2.5;//크기 조절 
+
+let painting = false;
+
+function stopPainting(){
+    painting = false;
+}
+
+function startPainting(){
+    painting = true;
+}
+
+function onMouseMove(event){//모든 움직임을 감지
+    const x = event.offsetX;
+    const y = event.offsetY;
+
+    if(!painting){//경로를 만든다.
+        ctx.beginPath();
+        ctx.moveTo(x, y);
+    }else{//그린다.
+        ctx.lineTo(x,y);
+        ctx.stroke();
+    }
+}
+
+function onMouseDown(event){
+    painting = true;
+
+}
+
+function onMouseUp(event){
+    stopPainting();
+}
+
+if(canvas){
+    canvas.addEventListener("mousemove", onMouseMove);
+    canvas.addEventListener("mousedown", startPainting);
+    canvas.addEventListener("mouseup", stopPainting);
+    canvas.addEventListener("mouseleave", stopPainting); //캔버스에 벗어났을때
+}
